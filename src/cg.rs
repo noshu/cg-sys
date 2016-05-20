06f3d7dcda083ef9b3bd3041693d785e97750fd4
noshu <naushad1290@gmail.com>
2016-05-21 00:43:24 +0600

    some bugfixes

--- src/cg.rs
+++ src/cg.rs
@@ -4,5 +4,5 @@ use libc::{c_double,c_int};
 extern "C"{
     pub fn CGFAM_(n:*const c_int,x:*mut c_double,f:*const c_double,g:*const c_double,d:*mut c_double,
     gold:*mut c_double,iprint:*const c_int,eps:*const c_double,w:*mut c_double,iflag:*mut c_int,irest:*mut c_int,
-    finish:*mut c_int);
+    method:*const c_int,finish:*mut c_int);
 }
