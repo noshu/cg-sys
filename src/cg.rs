5e2a27b4f546fa106111a1a44ad55334e96e36a6
noshu <naushad1290@gmail.com>
2016-05-20 23:32:47 +0600

    first commit

---
+++ src/cg.rs
@@ -0,0 +1,8 @@
+//CG+ binding
+use libc::{c_double,c_int};
+
+extern "C"{
+    pub fn CGFAM_(n:*const c_int,x:*mut c_double,f:*const c_double,g:*const c_double,d:*mut c_double,
+    gold:*mut c_double,iprint:*const c_int,eps:*const c_double,w:*mut c_double,iflag:*mut c_int,irest:*mut c_int,
+    finish:*mut c_int);
+}
